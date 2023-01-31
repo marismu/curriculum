b062f357fb6ffe2c2fb089b80d6e4a9cfb44dc20
marismu <marismunoz@abc.gob.ar>
2023-01-31 14:30:18 -0300

    terminando el proyecto

---
+++ index.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").addEventListener(`click`,function (){
+    console.log("desde Eventlistener");
+    document.getElementById("demo").innerHTML = "Amplia experiencia como auxiliar administrativa del sector financiero"
+});
+
+document.getElementById("boton_ocultar").addEventListener(`click`,function (){
+    document.getElementById("demo").style.display="none";
+    });
+
+
+
+
+
+    
